1e0fc24ae080a1ce658559496dbb793ff5124486
Lilesh Bawane <lileshrocking@gmail.com>
2024-01-25 00:28:50 +0530

    initial commit of homework1

---
+++ KiranAcademy/src/HW1/oneClass.java
@@ -0,0 +1,43 @@
+package HW1;
+
+public class oneClass {
+	
+	int sum(int a, int b) {
+		int ans1 = a + b;
+		return ans1;
+	}
+	
+	int sub(int a, int b) {
+		int ans2 = a-b;
+		return ans2;
+	}
+	
+	int multiply(int a, int b) {
+		int ans3 = a*b;
+		return ans3;
+	}
+	
+	float areaOfCircle(int pi, int r) {
+		float ans4 = pi * r * r;
+		return ans4;
+	}
+	
+	public static void main(String[] args) {
+		oneClass s = new oneClass();
+		int answer1 = s.sum(9, 33);
+		System.out.println(answer1);
+		
+		int answer2 =s.sub(9, 33);
+		System.out.println(answer2);
+		
+		int answer3 =s.multiply(9, 33);
+		System.out.println(answer3);
+
+		float answer4 =s.areaOfCircle(9, 33);
+		System.out.println(answer4);
+
+		
+
+
+	}
+}
