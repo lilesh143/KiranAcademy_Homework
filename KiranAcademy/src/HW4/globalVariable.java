e95e31ad48817238d8dabcdf3c7f33566aeb6c7a
Lilesh Bawane <lileshrocking@gmail.com>
2024-01-26 15:25:55 +0530

    adding global variable

---
+++ KiranAcademy/src/HW4/globalVariable.java
@@ -0,0 +1,48 @@
+package HW4;
+
+
+public class globalVariable {
+	// global variable
+	int a = 9;
+	int b = 33;
+
+	int sum() {
+		int ans1 = a + b;
+		return ans1;
+	}
+	
+	int sub() {
+		int ans2 = a-b;
+		return ans2;
+	}
+	
+	int multiply() {
+		int ans3 = a*b;
+		return ans3;
+	}
+	
+	float areaOfCircle() {
+		
+		float ans4 = pi * r * r;
+		return ans4;
+	}
+	
+	public static void main(String[] args) {
+		globalVariable s = new globalVariable();
+		
+		int answer1 = s.sum();
+		System.out.println(answer1);
+		
+		int answer2 =s.sub();
+		System.out.println(answer2);
+		
+		int answer3 =s.multiply();
+		System.out.println(answer3);
+
+		float answer4 =s.areaOfCircle();
+		System.out.println(answer4);
+
+	}
+
+
+}
